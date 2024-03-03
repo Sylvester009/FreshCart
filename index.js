aea0383089e6416586ed2e762eedf0f4b30e0c65
Sylvester009 <sasy260902@gmail.com>
2024-03-03 15:12:42 +0100

    massive revamp on fleshCart

---
+++ index.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 10000); // Change image every 8 seconds
+}
+
+// Add click event listener to each dot
+let dotElements = document.getElementsByClassName("dot");
+for (let i = 0; i < dotElements.length; i++) {
+  dotElements[i].addEventListener("click", function () {
+    slideIndex = i;
+    showSlides();
+  });
+}
